cad2ec86503ca4e1c052b2e9d953b93f65649dc8
salgadoIbacache <se.salgado@duocuc.cl>
2024-02-01 13:04:57 -0300

    Cambiando el nombre

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Testimony from './component/Testimony';
 
 function App() {
   return (
     <div className="App">
-      
+      <Testimony></Testimony>
     </div>
   );
 }

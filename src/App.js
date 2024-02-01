5ba83df8aca06e1c39d901b7103b0719cad3f850
salgadoIbacache <se.salgado@duocuc.cl>
2024-02-01 14:36:45 -0300

    Se agragan el front-end a Testimony.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,30 @@ function App() {
     <div className="App">
       <div className='main-container'>
       <h1>Esto es lo que dicen nuestros empleados</h1>
-      <Testimony></Testimony>
+      <Testimony 
+        name = "Sebastian Ibacache"
+        country = "Escocia"
+        image = "01"
+        employe = "Ingeniero Informatico"
+        company = "Amazon"
+        testimony = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel ipsum sit amet diam maximus aliquet eget a elit. Mauris ornare velit tellus, sed porttitor eros imperdiet tempor. Donec ullamcorper tortor id pulvinar pharetra"
+        ></Testimony>
+        <Testimony 
+        name = "Camila Montoya"
+        country = "Inglaterra"
+        image = "02"
+        employe = "Ingeniera de Software"
+        company = "Meta"
+        testimony = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel ipsum sit amet diam maximus aliquet eget a elit. Mauris ornare velit tellus, sed porttitor eros imperdiet tempor. Donec ullamcorper tortor id pulvinar pharetra"
+        ></Testimony>
+        <Testimony 
+        name = "Karina Salgado"
+        country = "Alemania"
+        image = "03"
+        employe = "Ingeniera de Software"
+        company = "Spotify"
+        testimony = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel ipsum sit amet diam maximus aliquet eget a elit. Mauris ornare velit tellus, sed porttitor eros imperdiet tempor. Donec ullamcorper tortor id pulvinar pharetra"
+        ></Testimony>
       </div>
     </div>
   );
--- src/component/Testimony.js
+++ src/component/Testimony.js
@@ -5,12 +5,12 @@ import "../styles/Testimony.css"
 function Testimony(props) {
 	return (
 		<div className="container-testimony">
-			<img className="image-testimony" src={require("../image/Image01.png")} 
+			<img className="image-testimony" src={require(`../image/Image-${props.image}.png`)} 
 			alt="student" ></img>
 			<div className="container-text-testimony">
 				<p className="name-testimony">{props.name} en {props.country}</p>
 				<p className="employer-testimony">{props.employe} en {props.company}</p>
-				<p className="text-testimony">{props.testimony}</p>
+				<p className="text-testimony">"{props.testimony}"</p>
 
 			</div>
 		</div>

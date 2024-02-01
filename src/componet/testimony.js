110b0e5ede086470a064187b259a146190404458
salgadoIbacache <se.salgado@duocuc.cl>
2024-02-01 12:37:19 -0300

    Create app

---
+++ src/componet/testimony.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function Testimony() {
+	return (
+		<div	className="container-testimony">
+			<img className="image-testimony"src={require("../image/image01.png")} 
+			alt="Image student" ></img>
+		</div>
+	);
+}

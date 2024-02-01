4db6070b44d334bd75f5ba66ddfbb11094558a2c
salgadoIbacache <se.salgado@duocuc.cl>
2024-02-01 13:57:46 -0300

    Agrego stile a Testimony.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Testimony from './component/Testimony';
+import  Testimony  from './component/Testimony';
 
 function App() {
   return (
     <div className="App">
+      <div className='main-container'>
+      <h1>Esto es lo que dicen nuestros empleados</h1>
       <Testimony></Testimony>
+      </div>
     </div>
   );
 }
--- src/component/Testimony.js
+++ src/component/Testimony.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/Testimony.css"
 
 
 function Testimony() {
